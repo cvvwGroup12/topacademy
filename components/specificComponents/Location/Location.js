9e5e8fa0dc948f704cabbaeec6cdc25f4f6fa7ff
106820015 <cc88030123@gmail.com>
2023-12-07 23:04:35 +0100

    fix location

--- components/specificComponents/Location/Location.js
+++ components/specificComponents/Location/Location.js
@@ -14,24 +14,25 @@ export default class Location extends Component {
 
 	render() {
 		return (
-			<div {...storyblokEditable(this.props.blok)}>
-				<Headermenu blok={this.props.menu.content}></Headermenu>
+			<div></div>
+			// <div {...storyblokEditable(this.props.blok)}>
+			// 	<Headermenu blok={this.props.menu.content}></Headermenu>
 
-				<main>
-					<Hero blok={this.props.blok} contentTypeTag="course" />
-					<div className={css["location-page__main-content"]}>
-						<div id="location-page__short-description" key="location-page__short-description" className={css["location-page__short-description"]}>
-							<section className={css["rich-text-section--with-navigator"]}>
-								<h2 className={css["rich-text-section__title"]}>Location Details</h2>
-								<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
-							</section>
-						</div>
-					</div>
-					{this.props.blok.additionalstuff?.map((nestedBlok) => (
-							<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-						))}
-				</main>
-			</div>
+			// 	<main>
+			// 		<Hero blok={this.props.blok} contentTypeTag="course" />
+			// 		<div className={css["location-page__main-content"]}>
+			// 			<div id="location-page__short-description" key="location-page__short-description" className={css["location-page__short-description"]}>
+			// 				<section className={css["rich-text-section--with-navigator"]}>
+			// 					<h2 className={css["rich-text-section__title"]}>Location Details</h2>
+			// 					<div className={css["rich-text-section__rich-text"]}>{RichTextToHTML({ document: this.props.blok.description })}</div>
+			// 				</section>
+			// 			</div>
+			// 		</div>
+			// 		{this.props.blok.additionalstuff.map((nestedBlok) => (
+			// 				<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+			// 			))}
+			// 	</main>
+			// </div>
 		);
 
 	}
