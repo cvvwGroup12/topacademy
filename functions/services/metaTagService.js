78730e5c51023f7abf605114eb3f11f02d4853cf
106820015 <cc88030123@gmail.com>
2024-01-11 22:36:13 +0100

    change web title

--- functions/services/metaTagService.js
+++ functions/services/metaTagService.js
@@ -63,16 +63,16 @@ function getDefaultTags() {
 		"googlebot": "all",
 		"robots": "all",
 		// OpenGraph
-		"og:site_name": "TopAcademy",
-		"og:title": "TopAcademy",
+		"og:site_name": "TopTravel",
+		"og:title": "TopTravel",
 		"og:description": description,
 		"og:image": imageUrl,
 		"og:type": "website",
 		// Twitter
-		"twitter:site": "@TopAcademy",
+		"twitter:site": "@TopTravel",
 		"twitter:card": "summary_large_image",
 		"twitter:image": imageUrl,
-		"twitter:title": "TopAcademy",
+		"twitter:title": "TopTravel",
 		"twitter:description": description
 	};
 }
